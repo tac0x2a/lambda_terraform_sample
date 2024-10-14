4250d171086570a7c2618af24c927530d90c647b
TAC <tac@tac42.net>
2024-10-14 17:52:17 +0900

    Fix lambda

--- modules/x-ray_sample/src/sample.py
+++ modules/x-ray_sample/src/sample.py
@@ -4,8 +4,8 @@ import json
 logger = getLogger(__name__)
 
 def handler(data, context):
-    logger.info(f"start handler {data=} {context=}")
     data_list = [ json.loads(json.loads(r["body"])["Message"]) for r in data["Records"] ]
+    logger.info(f"{data_list=}")
 
     for d in data_list:
       n = d["n"]
@@ -14,19 +14,19 @@ def handler(data, context):
       else:
           logger.info(f"{n} is odd")
 
-def _data2rec(data):
-    return {
-        "Records":[{
-            "body": json.dumps({
-                "Message": json.dumps(data)
-            })
-        }]
-    }
-
-
 if __name__ == "__main__":
     import logging
     import json
+
+    def _data2rec(data):
+      return {
+          "Records":[{
+              "body": json.dumps({
+                  "Message": json.dumps(data)
+              })
+          }]
+      }
+
     ch = logging.StreamHandler()
     logger.addHandler(ch)
     logger.setLevel(logging.INFO)
