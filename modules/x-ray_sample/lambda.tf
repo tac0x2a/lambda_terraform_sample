8935ffe9466281949562d06d867fb9dad89c8af4
TAC <tac@tac42.net>
2024-10-15 23:10:03 +0900

    Add lambda layer

--- modules/x-ray_sample/lambda.tf
+++ modules/x-ray_sample/lambda.tf
@@ -1,3 +1,4 @@
+## IAM Role
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect  = "Allow"
@@ -8,7 +9,6 @@ data "aws_iam_policy_document" "assume_role" {
     }
   }
 }
-
 data "aws_iam_policy_document" "inline_policy" {
   statement {
     effect = "Allow"
@@ -19,7 +19,6 @@ data "aws_iam_policy_document" "inline_policy" {
     ]
     resources = ["arn:aws:logs:*:*:*"]
   }
-
   statement {
     effect = "Allow"
     actions = [
@@ -30,8 +29,6 @@ data "aws_iam_policy_document" "inline_policy" {
     resources = [aws_sqs_queue.x-ray_sample_queue.arn]
   }
 }
-
-
 resource "aws_iam_role" "iam_for_x-ray_lambda" {
   name               = "iam_for_x-ray_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
@@ -41,14 +38,44 @@ resource "aws_iam_role" "iam_for_x-ray_lambda" {
   }
 }
 
+## Lambda Layer
+locals {
+  layer_work_path = "/tmp/x-ray_sample/x-ray_sample_layer"
+}
+resource "terraform_data" "x-ray_lambda_layer_pip_install" {
+  input = local.layer_work_path
+  triggers_replace = {
+    file_content = md5(file("${path.module}/src/requirements.txt"))
+    # file_existance = md5(file("${layer_work_path}/python/requirements.txt"))
+    # timestamp = timestamp()
+  }
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command     = <<EOF
+      [[ -d "${local.layer_work_path}/python" ]] && rm -rf "${local.layer_work_path}/python"
+      pip install -r '${path.module}/src/requirements.txt' --target='${local.layer_work_path}/python' --no-cache-dir
+    EOF
+  }
+}
+data "archive_file" "x-ray_lambda_layer" {
+  type        = "zip"
+  source_dir  = terraform_data.x-ray_lambda_layer_pip_install.output
+  output_path = "${local.layer_work_path}.zip"
+}
+resource "aws_lambda_layer_version" "x-ray_lambda_layer" {
+  filename            = data.archive_file.x-ray_lambda_layer.output_path
+  layer_name          = "x-ray_sample_layer"
+  source_code_hash    = data.archive_file.x-ray_lambda_layer.output_base64sha256
+  compatible_runtimes = ["python3.11"]
+}
+
+## Lambda Function
 data "archive_file" "x-ray_lambda" {
   type = "zip"
-
   dynamic "source" {
     for_each = toset([
       "sample.py"
     ])
-
     content {
       content  = file("${path.module}/src/${source.value}")
       filename = basename(source.value)
@@ -58,22 +85,21 @@ data "archive_file" "x-ray_lambda" {
 }
 
 resource "aws_lambda_function" "x-ray_lambda" {
-  filename      = data.archive_file.x-ray_lambda.output_path
-  function_name = "x-ray_sample_function"
-  role          = aws_iam_role.iam_for_x-ray_lambda.arn
-  handler       = "sample.handler"
-
+  filename         = data.archive_file.x-ray_lambda.output_path
+  function_name    = "x-ray_sample_function"
+  role             = aws_iam_role.iam_for_x-ray_lambda.arn
+  handler          = "sample.handler"
   source_code_hash = data.archive_file.x-ray_lambda.output_base64sha256
-
-  runtime = "python3.11"
-
+  runtime          = "python3.11"
   environment {
     variables = {}
   }
   logging_config {
     log_format = "JSON"
   }
+  layers = [aws_lambda_layer_version.x-ray_lambda_layer.arn]
 }
+
 
 resource "aws_lambda_event_source_mapping" "x-ray_sqs2lambda_mapping" {
   event_source_arn = aws_sqs_queue.x-ray_sample_queue.arn
--- modules/x-ray_sample/src/sample.py
+++ modules/x-ray_sample/src/sample.py
@@ -1,31 +1,33 @@
 from logging import getLogger
 import json
+from sympy import sieve
 
 logger = getLogger(__name__)
 
+
 def handler(data, context):
-    data_list = [ json.loads(json.loads(r["body"])["Message"]) for r in data["Records"] ]
+    # import os
+    # logger.info(f"{os.listdir('/opt')=}")
+    # if os.path.isdir("/opt/python"):
+    #     logger.info(f"{os.listdir('/opt/python')=}")
+
+    data_list = [json.loads(json.loads(r["body"])["Message"]) for r in data["Records"]]
     logger.info(f"{data_list=}")
 
     for d in data_list:
-      n = d["n"]
-      if n % 2 == 0:
-          logger.info(f"{n} is even")
-      else:
-          logger.info(f"{n} is odd")
+        n = d["n"]
+        if n in sieve:
+            logger.info(f"{n} is prime")
+        else:
+            logger.info(f"{n} is NOT prime")
+
 
 if __name__ == "__main__":
     import logging
     import json
 
     def _data2rec(data):
-      return {
-          "Records":[{
-              "body": json.dumps({
-                  "Message": json.dumps(data)
-              })
-          }]
-      }
+        return {"Records": [{"body": json.dumps({"Message": json.dumps(data)})}]}
 
     ch = logging.StreamHandler()
     logger.addHandler(ch)
